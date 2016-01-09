266521291b3cb9a43afbee8c57d155ee331a1a9e
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-01-09 01:04:00 +0100

    Fix problem with validation and add unit test for it

--- src/test/java/org/home/spring/mvc/controller/RegisterUserControllerTest.java
+++ src/test/java/org/home/spring/mvc/controller/RegisterUserControllerTest.java
@@ -8,10 +8,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -20,8 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(MockitoJUnitRunner.class)
-@WebAppConfiguration
 public class RegisterUserControllerTest {
+    @Mock private        Validator              validator;
     @Mock private        UsersRepository        usersRepository;
     @InjectMocks private RegisterUserController controller;
 
@@ -29,7 +34,9 @@ public class RegisterUserControllerTest {
 
     @Before
     public void setUp() throws Exception {
-        mockMvc = standaloneSetup(controller).build();
+        mockMvc = standaloneSetup(controller)
+                .setValidator(validator)
+                .build();
     }
 
     @Test
@@ -50,5 +57,26 @@ public class RegisterUserControllerTest {
                .andExpect(redirectedUrl("/user/show/Jack"));
 
         verify(usersRepository).save(user);
+    }
+
+    @Test
+    public void shouldRegistrationFormBeShown() throws Exception {
+        when(validator.supports(any(Class.class))).thenReturn(true);
+        doAnswer(invocation -> {
+            Object secondArgument = invocation.getArguments()[1];
+            Errors errors = (Errors) secondArgument;
+
+            errors.reject("my error");
+
+            return null;
+        }).when(validator).validate(any(Object.class), any(Errors.class));
+
+        mockMvc.perform(post("/user/register")
+                                .param("name", "J")
+                                .param("surname", "B"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("registerForm"));
+
+        verify(usersRepository, never()).save(any(User.class));
     }
 }
